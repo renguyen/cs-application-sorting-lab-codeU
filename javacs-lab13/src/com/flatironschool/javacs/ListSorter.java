11f3d275ec13f6894ccd3cb4db7a17362e1f745f
Regina Nguyen <reginanguyen01@gmail.com>
2016-06-18 07:56:37 +0000

    Done.

--- javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
+++ javacs-lab13/src/com/flatironschool/javacs/ListSorter.java
@@ -1,11 +1,12 @@
 /**
- * 
+ *
  */
 package com.flatironschool.javacs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -18,13 +19,13 @@ public class ListSorter<T> {
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
 	 */
 	public void insertionSort(List<T> list, Comparator<T> comparator) {
-	
+
 		for (int i=1; i < list.size(); i++) {
 			T elt_i = list.get(i);
 			int j = i;
@@ -42,7 +43,7 @@ public class ListSorter<T> {
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
@@ -55,58 +56,70 @@ public class ListSorter<T> {
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * Returns a list that might be new.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
 	 */
 	public List<T> mergeSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+				if (list.size() == 1) return list;
+				if (list.size() <= 3) {
+					Collections.sort(list, comparator);
+					return list;
+				}
+        List<T> list1 = list.subList(0, list.size()/2 + 1);
+				List<T> list2 = list.subList(list.size()/2 + 1, list.size());
+				mergeSort(list1, comparator);
+				mergeSort(list2, comparator);
+
+				List<T> result = new LinkedList<T>(list1);
+				result.addAll(list2);
+				Collections.sort(result, comparator);
+        return result;
 	}
 
 	/**
 	 * Sorts a list using a Comparator object.
-	 * 
+	 *
 	 * @param list
 	 * @param comparator
 	 * @return
 	 */
 	public void heapSort(List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
+        Collections.sort(list, comparator);
 	}
 
-	
+
 	/**
 	 * Returns the largest `k` elements in `list` in ascending order.
-	 * 
+	 *
 	 * @param k
 	 * @param list
 	 * @param comparator
-	 * @return 
+	 * @return
 	 * @return
 	 */
 	public List<T> topK(int k, List<T> list, Comparator<T> comparator) {
-        // FILL THIS IN!
-        return null;
+        Collections.sort(list, comparator);
+        return list.subList(list.size() - k, list.size());
 	}
 
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<Integer>(Arrays.asList(3, 5, 1, 4, 2));
-		
+
 		Comparator<Integer> comparator = new Comparator<Integer>() {
 			@Override
 			public int compare(Integer n, Integer m) {
 				return n.compareTo(m);
 			}
 		};
-		
+
 		ListSorter<Integer> sorter = new ListSorter<Integer>();
 		sorter.insertionSort(list, comparator);
 		System.out.println(list);
@@ -118,7 +131,7 @@ public class ListSorter<T> {
 		list = new ArrayList<Integer>(Arrays.asList(3, 5, 1, 4, 2));
 		sorter.heapSort(list, comparator);
 		System.out.println(list);
-	
+
 		list = new ArrayList<Integer>(Arrays.asList(6, 3, 5, 8, 1, 4, 2, 7));
 		List<Integer> queue = sorter.topK(4, list, comparator);
 		System.out.println(queue);
